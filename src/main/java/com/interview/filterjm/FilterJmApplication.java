695cd9a799f21f60a3aed96e6433cb0a1d3ac322
Jamal Monger <jmonger89@gmail.com>
2021-02-03 18:02:18 -0500

    Initial commit. Adds java spring boilerplate.

---
+++ src/main/java/com/interview/filterjm/FilterJmApplication.java
@@ -0,0 +1,13 @@
+package com.interview.filterjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterJmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterJmApplication.class, args);
+	}
+
+}
